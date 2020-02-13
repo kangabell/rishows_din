dea1429e07aff1d15776cd5248515fa0a2056511
kangabell <k@kangabell.co>
2020-02-13 14:40:15 -0500

    add more metadata to single event page

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -2,8 +2,24 @@
 
 	<?php while ( have_posts() ) :  the_post(); ?>
 
-		<?php echo tribe_get_start_date(); ?>
-		<?php the_title('<h1>', '</h1>') ?>
+		<header class="page-header">
+			<?php echo tribe_get_start_date(); ?>
+
+			<?php the_title('<h1>', '</h1>') ?>
+
+			<?php if ( tribe_get_cost() ) : ?>
+				<span class="event-meta event-cost">
+					<?php echo tribe_get_cost( $post_id, true ); ?>
+				</span>
+			<?php endif; ?>
+
+			<?php if ( tribe_get_event_meta() ) : ?>
+				<span class="event-meta event-age">
+					<?php echo tribe_get_event_meta( $postId, '_ecp_custom_2' ); ?>
+				</span>
+			<?php endif; ?>
+		</header>
+
 		<?php the_content(); ?>
 		<?php the_post_thumbnail(); ?>
 		<p>
