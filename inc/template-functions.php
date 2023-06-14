64459fdb7e2ec9ca6d174bad54ff3d35c4aefba0
kangabell <k@kangabell.co>
2023-06-13 22:35:43 -0400

    always use 'no-sidebar' layout

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -17,10 +17,8 @@ function rishows_din_body_classes( $classes ) {
 		$classes[] = 'hfeed';
 	}
 
-	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
-	}
+	// Adds a class of no-sidebar site-wide
+	$classes[] = 'no-sidebar';
 
 	return $classes;
 }
