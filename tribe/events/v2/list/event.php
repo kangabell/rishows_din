dce9f801392e152df149b3588618966453933706
kangabell <k@kangabell.co>
2023-06-13 21:59:44 -0400

    Tribe v2 update: convert list-view event template

--- tribe/events/v2/list/event.php
+++ tribe/events/v2/list/event.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * List View Single Event
- * This file contains one event in the list view
+ * View: List Event
  *
- * @version 4.6.19
- * (heavily modified)
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/v2/list/event.php
  *
+ * See more documentation about our views templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 5.0.0
+ *
+ * @var WP_Post $event The event post object with properties added by the `tribe_get_event` function.
+ *
+ * @see tribe_get_event() For the format of the event object.
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	die( '-1' );
-}
 
 ?>
 
