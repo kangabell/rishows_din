e50365a899e0cba69af3f62bb0cba7023068861c
kangabell <k@kangabell.co>
2023-06-14 17:28:32 -0400

    style subscribe/export button/dropdown

--- tribe/events/v2/components/subscribe-links/list.php
+++ tribe/events/v2/components/subscribe-links/list.php
@@ -22,9 +22,9 @@ if ( empty( $items ) ) {
 	<div class="tribe-events-c-subscribe-dropdown">
 		<div class="tribe-common-c-btn-border tribe-events-c-subscribe-dropdown__button" tabindex="0">
 			<button class="tribe-events-c-subscribe-dropdown__button-text">
-				<?php echo esc_html__( 'Subscribe to calendar', 'the-events-calendar' ); ?>
+				<?php echo esc_html__( 'Subscribe/Export List', 'the-events-calendar' ); ?>
+				<?php $this->template( 'components/icons/caret-down', [ 'classes' => [ 'tribe-events-c-subscribe-dropdown__button-icon' ] ] ); ?>
 			</button>
-			<?php $this->template( 'components/icons/caret-down', [ 'classes' => [ 'tribe-events-c-subscribe-dropdown__button-icon' ] ] ); ?>
 		</div>
 		<div class="tribe-events-c-subscribe-dropdown__content">
 			<ul class="tribe-events-c-subscribe-dropdown__list" tabindex="0">
