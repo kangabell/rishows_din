2759ef984e8b687519a291a5d1580a10a378b511
kangabell <k@kangabell.co>
2023-06-17 16:06:08 -0400

    overhaul list item layout: everything fits on one line, title truncated with ellipsis, fancy gradient link underline style

--- tribe/events/v2/list/event.php
+++ tribe/events/v2/list/event.php
@@ -18,37 +18,39 @@
 
 ?>
 
-<span class="event-meta event-date">
-	<?php echo tribe_get_start_date( $event, false ); ?>	
-</span>
+<div class="type-tribe_events">
 
-<h3 class="event-meta event-title">
-	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-</h3>
-
-<span class="event-meta event-venue">
-	<?php echo tribe_get_venue(); ?>
-</span>
-
-<?php if ( tribe_get_start_time() ) : ?>
-	<span class="event-meta event-time">
-		<?php
-		$time = tribe_get_start_time();
-		echo preg_replace( "/(:)0{2} /", "", $time ); // remove ":00 "
-		?>
+	<span class="event-meta event-date">
+		<?php echo tribe_get_start_date( $event, false ); ?>
 	</span>
-<?php endif; ?>
 
-<?php if ( tribe_get_cost() ) : ?>
-	<span class="event-meta event-cost">
-		<?php echo tribe_get_cost( $post_id, true ); ?>
+	<h3 class="event-meta event-title">
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</h3>
+
+	<?php if ( tribe_get_start_time() ) : ?>
+		<span class="event-meta event-time">
+			<?php
+			$time = tribe_get_start_time();
+			echo preg_replace( "/(:)0{2} /", "", $time ); // remove ":00 "
+			?>
+		</span>
+	<?php endif; ?>
+
+	<?php if ( tribe_get_cost() ) : ?>
+		<span class="event-meta event-cost">
+			<?php echo tribe_get_cost( $post_id, true ); ?>
+		</span>
+	<?php endif; ?>
+
+	<?php if ( tribe_get_event_meta() ) : ?>
+		<span class="event-meta event-age">
+			<?php echo tribe_get_event_meta( $postId, '_ecp_custom_2' ); ?>
+		</span>
+	<?php endif; ?>
+
+	<span class="event-meta event-venue">
+		<?php echo tribe_get_venue(); ?>
 	</span>
-<?php endif; ?>
 
-<?php if ( tribe_get_event_meta() ) : ?>
-	<span class="event-meta event-age">
-		<?php echo tribe_get_event_meta( $postId, '_ecp_custom_2' ); ?>
-	</span>
-<?php endif; ?>
-
-<hr>
+</div>
